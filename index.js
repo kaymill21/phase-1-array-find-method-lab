0178335b2c99168419785e8af7b698b59a0465b3
kayla <kay.ann2000@gmail.com>
2022-03-17 11:53:43 -0600

    commit tree a3cbc2851c7e516246b045c7d2b96d26cc361d75

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+// const record = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     //...
+//   ]
+ // function superbowlWin(record) {
+    //  for(year of record)
+    //  return record.find("w")
+  
+      
+         //return record.find( year === 'W') 
+  //}
+
+  const superbowlWin = (record) => {
+     const winResult = record.find(record => record.result === 'W') 
+  return !!winResult ? winResult.year : undefined;
+  }
